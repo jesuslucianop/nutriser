521c376adeb596c67e4f63bfa143361d70bae839
jesuslucianop <luciano-peralta@hotmail.com>
2018-05-22 23:43:10 -0400

    seccond commit

--- Index.php
+++ Index.php
@@ -3,7 +3,15 @@ include "./Class/Database.php";
 include "./Template/header.php";
 ?>
 <div class="container">
-<div class=""></div>
+<img src="./Assets/Images/Logo.png" >
+<div class="row">
+
+<div class="col col-md-6 col-md-offset-6">
+	
+	<h1>Sistema de servicio de nutriser</h1>
+	
+</div>
+</div>
 <div>
 
 
